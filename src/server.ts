1f7ad64adb5c8643339a5d8106d9eb041e0f981f
Amaechi Precious <preciousamaechi111@yahoo.com>
2025-02-01 00:12:50 +0100

    debug datetime

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
 const email = "preciousamaechi111@yahoo.com";
+const date = new Date().toISOString();
 
 app.get("/api/", (req: Request, res: Response) => {
-  res.json({ email, current_datetime: Date.now(), github_url: "" });
+  res.json({
+    email,
+    current_datetime: date,
+    github_url: "https://github.com/Precious-c/hng_Stage_0",
+  });
 });
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
